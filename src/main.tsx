eb0e2476761c698c91cc9de48833283027f1157d
perperhcy <58731865+perperhcy@users.noreply.github.com>
2024-04-19 12:08:29 +0800

    3

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
+// this manifest is used temporarily for development purposes
+const manifestUrl = 'https://perperhcy.github.io/my-twa/tonconnect-manifest.json';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>,
+)
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
